84091da0c8f01b772487057cca96ebc52e7c2a8a
Alexey <mister.skorpion4ik@mail.ru>
2025-12-11 12:32:27 +0500

    realise crud models

--- postify/views.py
+++ postify/views.py
@@ -1,12 +1,46 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        post = self.get_object()
+        if post.author != self.request.user and not self.request.user.is_staff:
+            raise permissions.PermissionDenied("У вас нет прав редактировать этот пост.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user and not self.request.user.is_staff:
+            raise permissions.PermissionDenied("У вас нет прав удалять этот пост.")
+        instance.delete()
+
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        comment = self.get_object()
+        if comment.author != self.request.user and not self.request.user.is_staff:
+            raise permissions.PermissionDenied("У вас нет прав редактировать этот комментарий.")
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user and not self.request.user.is_staff:
+            raise permissions.PermissionDenied("У вас нет прав удалять этот комментарий.")
+        instance.delete()
--- users/views.py
+++ users/views.py
@@ -1,7 +1,18 @@
-from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, permissions
 from .models import User
 from .serializers import UserSerializer
 
+
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def perform_update(self, serializer):
+        if self.request.user.is_staff or self.request.user == serializer.instance:
+            serializer.save()
+        else:
+            raise permissions.PermissionDenied("У вас нет прав редактировать этого пользователя.")
